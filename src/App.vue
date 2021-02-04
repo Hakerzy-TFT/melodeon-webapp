64b214f98aeb8c3e221179e0ff0c5a32b19174b7
RRobak <rafal.robak@interia.pl>
2021-02-04 22:44:29 +0100

    Added parts of main page view

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <Home />
+  </div>
+</template>
+<script>
+import Home from "./components/Home.vue";
+export default {
+  name: "App",
+  components: {
+    Home,
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin: 0%;
+  padding: 0%;
+  width: 100%;
+  height: 100%
+}
+</style>
---
+++ src/components/Home.vue
@@ -0,0 +1,49 @@
+<template>
+  <div id="Home">
+    <div id="background"></div>
+    <div id="conbackground">
+      <div id="text">
+        <input type="textarea" id="token" />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Home",
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+#background {
+  background-image: url("../assets/home_background.png");
+  background-size: cover;
+  width: 100vw;
+  height: 100vh;
+  opacity: 0.9;
+  position: absolute;
+  top: 0;
+  left: 0;
+  filter: blur(6px);
+}
+#conbackground {
+  background-image: url("../assets/home_background.png");
+  background-size: cover;
+  width: 60%;
+  height: 90%;
+  opacity: 0.9;
+  position: absolute;
+  top: 5%;
+  left: 20%;
+  border-radius: 1000px;
+}
+#token {
+  vertical-align: middle; 
+  top: 80%;
+  height: 30px;
+  width: 40%;
+  border-radius: 500px;
+}
+</style>
